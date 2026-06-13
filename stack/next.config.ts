9f92af1db06cf6f15790e371e1f9899ee1385804
Your Name <you@example.com>
2026-06-13 10:50:39 +0530

    changes done in vercel file

--- stack/next.config.ts
+++ stack/next.config.ts
@@ -9,10 +9,7 @@ const nextConfig: NextConfig = {
     // This is safe because we use JSX files for API/context layers
     ignoreBuildErrors: true,
   },
-  eslint: {
-    // Deployment will succeed even if there are lint warnings
-    ignoreDuringBuilds: true,
-  },
+
 };
 
 export default nextConfig;
